1b81a6128c15ff6489169b66f6cc67aefe2a179e
rian33svan <96176662+RianErick@users.noreply.github.com>
2022-11-06 16:10:18 -0300

    Introducao Poo 01

---
+++ src/POO_01/Entites/Triangulo.java
@@ -0,0 +1,15 @@
+package POO_01.Entites;
+
+
+//Classe Entidade
+public class Triangulo {
+    public double a;
+    public double b;
+    public double c;
+
+    //Funçao Retorna Operaçao
+    public double resultadoOp () {
+        return a * a + b - c;
+    }
+
+}
